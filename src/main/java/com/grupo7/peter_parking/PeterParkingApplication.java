31e688422660d921a317c645913384829fd8a969
Vinicius <viniciusalves_96@hotmail.com>
2025-01-06 21:21:11 -0300

    initial commit

---
+++ src/main/java/com/grupo7/peter_parking/PeterParkingApplication.java
@@ -0,0 +1,13 @@
+package com.grupo7.peter_parking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeterParkingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeterParkingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/grupo7/peter_parking/PeterParkingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo7.peter_parking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeterParkingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
